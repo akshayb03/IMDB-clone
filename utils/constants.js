53e2231029b4b287ff302048381d27817d4a82a2
akshayb03 <akshaybhati003@gmail.com>
2025-04-21 17:19:43 +0530

    fix: change backend base url

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const DEFAULT_IMAGE_URL="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-// export const BASE_URL="https://imdb-clone-backend-7exc.onrender.com"
-export const BASE_URL="http://localhost:8000"
+export const BASE_URL="https://imdb-clone-backend-7exc.onrender.com"
+// export const BASE_URL="http://localhost:8000"
